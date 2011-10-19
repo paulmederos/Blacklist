373ff8ea21c6a33a85e124fb3306ba9e1a227e29
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-10-19 11:40:24 -0700

    rails new prime_enterprise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeEnterprise::Application.config.secret_token = 'c5328d51fffda1f92f5dfe17246161976b7fc996665aa094897c2fea45268818bbcd205e4d1479243a9c8dcc796f5c6146c50d873a797370daac69a8afcfdf0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PrimeEnterprise::Application.config.session_store :cookie_store, key: '_prime_enterprise_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PrimeEnterprise::Application.config.session_store :active_record_store
