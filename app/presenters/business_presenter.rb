9f27a2552ecba02e911dd0765e7e05c6ae142f9b
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-10-23 13:24:03 -0400

    got businesses page behaving more like the mockup

--- app/presenters/business_presenter.rb
+++ app/presenters/business_presenter.rb
@@ -3,4 +3,16 @@ class BusinessPresenter < DelegatePresenter::Base
   def biz_greeting
     "Hello, #{self.name}"
   end
+
+  def full_address
+    "#{self.street_address}, #{self.city}, #{self.state} #{self.zip}"
+  end
+
+
+  def complaint_button
+    s('<button class="btn primary ">',
+      helpers.link_to("File Complaint", helpers.new_complaint_path),
+      '</button>'
+    )
+  end
 end
