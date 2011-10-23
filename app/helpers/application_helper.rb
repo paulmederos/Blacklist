943255e26e986488160e4e6179ede97c482bc444
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-10-23 11:26:35 -0400

    layouts that inherit from application.html
    application.html also has a content_for helper to control the header text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def parent_layout(layout)
+    @view_flow.set(:layout,output_buffer)
+    self.output_buffer = render(:file => "layouts/#{layout}")
+  end
+
+  def title_or_default
+    @title_override || "#{controller.controller_name.titleize}"
+  end
 end
