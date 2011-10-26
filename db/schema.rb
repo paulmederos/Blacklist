874cbea37263ba9560804594ba330f1d90b01272
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-10-26 17:08:08 -0400

    set up gmaps4rails boilerplate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111020200357) do
+ActiveRecord::Schema.define(:version => 20111026193041) do
 
   create_table "businesses", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(:version => 20111020200357) do
     t.string   "zip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.boolean  "gmaps"
   end
 
   create_table "complaints", :force => true do |t|
