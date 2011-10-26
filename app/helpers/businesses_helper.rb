69c343ae2a7605262e0057dddd7d0f1cbd3f4a49
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-10-26 17:08:35 -0400

    set up, and style, gmaps4rails to fit our liking

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -1,2 +1,10 @@
 module BusinessesHelper
+
+  def our_gmaps4rails(json)
+    # { "detect_location" => true, "center_on_user" => true, "zoom" => 3}
+    gmaps("map_options" => {:auto_adjust => true, :auto_zoom => false, :zoom => 16},
+          "markers" => { "data" => json }
+    )
+  end
+
 end
